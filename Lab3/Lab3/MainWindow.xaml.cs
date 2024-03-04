0b93e1398ce416727793473dc989d90b1a49aa60
mishadiatel <mishadiatel13@gmail.com>
2024-03-04 13:33:12 +0200

    added UI and programm logic

--- Lab3/Lab3/MainWindow.xaml.cs
+++ Lab3/Lab3/MainWindow.xaml.cs
@@ -1,13 +1,60 @@
-﻿namespace Lab3
+﻿using System;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+
+
+
+namespace Lab3
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow
+    public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        // Шифрування тексту
+        private void EncryptButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (ShiftTextBox.Text.Trim().Length == 0 || !int.TryParse(ShiftTextBox.Text, out _))
+            {
+                MessageBox.Show("Please enter shift");
+                return;
+            }
+            int shift = int.Parse(ShiftTextBox.Text);
+            string plaintext = InputTextBox.Text;
+            string ciphertext = Encrypt(plaintext, shift);
+            OutputTextBox.Text = ciphertext;
+        }
+
+        // Розшифрування тексту
+        private void DecryptButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (ShiftTextBox.Text.Trim().Length == 0 || !int.TryParse(ShiftTextBox.Text, out _))
+            {
+                MessageBox.Show("Please enter shift");
+                return;
+            }
+            int shift = int.Parse(ShiftTextBox.Text);
+            string ciphertext = InputTextBox.Text;
+            string plaintext = Decrypt(ciphertext, shift);
+            OutputTextBox.Text = plaintext;
+        }
+
+        // Метод шифрування тексту шифром Цезаря
+        
+        public string Encrypt(string input, int key) {  
+            string output = string.Empty;  
+  
+            foreach(char ch in input)  
+                output += (char)((ch + key));  
+  
+            return output;  
+        }  
+  
+        public string Decrypt(string input, int key) {  
+            return Encrypt(input,  -key);  
         }
     }
 }
