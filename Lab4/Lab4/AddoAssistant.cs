c042c03435bbf16c2c5402f9223a358123a6f00b
mishadiatel <mishadiatel13@gmail.com>
2024-04-28 14:25:53 +0300

    added lab 4

---
+++ Lab4/Lab4/AddoAssistant.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows;
+using MySql.Data.MySqlClient;
+using MySql.Data;
+
+
+namespace Lab4
+{
+    public class AddoAssistant
+    {
+        string connectionString = "Server=localhost;Uid=root;Pwd=password;database=dpgiproductsdb";
+        
+        DataTable dt = null; 
+        public DataTable TableLoad()
+        {
+            if (dt != null) return dt;
+            dt = new DataTable();
+            using (MySqlConnection сonnection = new MySqlConnection(connectionString)) 
+            {
+                MySqlCommand command = сonnection.CreateCommand(); 
+                MySqlDataAdapter adapter = new MySqlDataAdapter(command); 
+                command.CommandText = "Select id, article, unit, quantity, price from products";
+                try
+                {
+                    adapter.Fill(dt);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Помилка підключення до БД");
+                }
+            }
+            return dt;
+        }
+
+        public void DeleteItem(String id)
+        {
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            MySqlCommand deleteCommand = new MySqlCommand($"delete from products where id = '{id}'", connection);
+            connection.Open();
+            deleteCommand.ExecuteNonQuery();
+            connection.Close();
+        }
+        
+        public void UpdateItem(String id, String article, String unit, String quantity, String price)
+        {
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            MySqlCommand deleteCommand = new MySqlCommand($"update products set article = '{article}', unit='{unit}', quantity='{quantity}', price='{price}' where id = '{id}'", connection);
+            connection.Open();
+            deleteCommand.ExecuteNonQuery();
+            connection.Close();
+        }
+        
+        public void CreateItem(String article, String unit, String quantity, String price)
+        {
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            MySqlCommand deleteCommand = new MySqlCommand($"insert into products (article, unit, quantity, price) values ('{article}', '{unit}', '{quantity}', '{price}')", connection);
+            connection.Open();
+            deleteCommand.ExecuteNonQuery();
+            connection.Close();
+        }
+
+    }
+}
--- Lab4/Lab4/MainWindow.xaml.cs
+++ Lab4/Lab4/MainWindow.xaml.cs
@@ -1,4 +1,9 @@
-﻿namespace Lab4
+﻿using System;
+using System.Data;
+using System.Windows;
+using MySql.Data.MySqlClient;
+
+namespace Lab4
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -8,6 +13,76 @@
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+        
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadTable();
+        }
+
+        private void LoadTable()
+        {
+            AddoAssistant myTable= new AddoAssistant();
+                        list.SelectedIndex = 0;
+                        list.Focus();
+                        list.DataContext = myTable.TableLoad();
+        }
+        
+
+        private void ButtonCreate_OnClick(object sender, RoutedEventArgs e)
+        {
+            var selectedItem = list.SelectedItem as DataRowView;
+            var article  = Article.Text;
+            var unit = Unit.Text;
+            var quantity = Quantity.Text;
+            var price = Price.Text;
+            new AddoAssistant().CreateItem(article,unit, quantity, price);
+            MessageBox.Show("Created successfully");
+            
+            LoadTable();
+        }
+
+        private void ButtonUpdate_OnClick(object sender, RoutedEventArgs e)
+        {
+            var selectedItem = list.SelectedItem as DataRowView;
+            if (selectedItem is null)
+            {
+                MessageBox.Show(messageBoxText: "Select item if you want update it, please.",
+                    caption: "Error!",
+                    button: MessageBoxButton.OK,
+                    icon: MessageBoxImage.Error,
+                    defaultResult: MessageBoxResult.OK);
+             
+                return;
+            }
+            var id = selectedItem["id"].ToString();
+            var article  = Article.Text;
+            var unit = Unit.Text;
+            var quantity = Quantity.Text;
+            var price = Price.Text;
+            new AddoAssistant().UpdateItem(id, article,unit, quantity, price);
+            MessageBox.Show("Updated successfully");
+            LoadTable();
+        }
+
+        private void ButtonDelete_OnClick(object sender, RoutedEventArgs e)
+        {
+            var selectedItem = list.SelectedItem as DataRowView;
+            if (selectedItem is null)
+            {
+                MessageBox.Show(messageBoxText: "Select item if you want delete it, please.",
+                    caption: "Error!",
+                    button: MessageBoxButton.OK,
+                    icon: MessageBoxImage.Error,
+                    defaultResult: MessageBoxResult.OK);
+            
+                return;
+            }
+            var id = selectedItem["id"].ToString();
+            new AddoAssistant().DeleteItem(id);
+            MessageBox.Show("Deleted successfully");
+            LoadTable();
         }
     }
 }
